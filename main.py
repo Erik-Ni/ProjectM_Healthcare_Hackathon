9f35b9e95f70b864c85ad2ec44f935e734dfba60
Duong Vu <45385394+vudh1@users.noreply.github.com>
2021-05-21 15:05:11 -0700

    refractor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 from configparser import ConfigParser
-from utils.video_access import video_access
+from utils.video_stream import video_stream
 
 ############################################################################################################################################
 
@@ -10,23 +10,24 @@ parser.read('config.ini')
 
 config = dict()
 
-config['EMOTION_API'] = parser.get('NETWORK', 'EMOTION_API')
-config['MOOD_API'] = parser.get('NETWORK', 'MOOD_API')
+config['EMOTION_API'] = str(parser.get('NETWORK', 'EMOTION_API'))
+config['MOOD_API'] = str(parser.get('NETWORK', 'MOOD_API'))
 config['REQUEST_TIMEOUT'] = int(parser.get('NETWORK', 'REQUEST_TIMEOUT'))
 config['FRAMES_PER_REQUEST'] = int(parser.get('NETWORK', 'FRAMES_PER_REQUEST'))
 
-config['PROTOTXT_DIRECTORY'] = parser.get('DIRECTORIES', 'PROTOTXT_DIRECTORY')
-config['CAFFEMODEL_DIRECTORY'] = parser.get('DIRECTORIES', 'CAFFEMODEL_DIRECTORY')
+config['PROTOTXT_DIRECTORY'] = str(parser.get('DIRECTORIES', 'PROTOTXT_DIRECTORY'))
+config['CAFFEMODEL_DIRECTORY'] = str(parser.get('DIRECTORIES', 'CAFFEMODEL_DIRECTORY'))
 
-config['EMOTIONS'] = json.loads(parser.get('DATA', 'EMOTIONS'))
-config['MOODS'] = json.loads(parser.get('DATA', 'MOODS'))
+config['EMOTIONS'] = list(json.loads(parser.get('DATA', 'EMOTIONS')))
+config['MOODS'] = list(json.loads(parser.get('DATA', 'MOODS')))
 
 config['FONT_STYLE'] = int(parser.get('OPENCV', 'FONT_STYLE'))
 config['FONT_SCALE'] = float(parser.get('OPENCV', 'FONT_SCALE'))
-config['COLOR'] = (int(parser.get('OPENCV', 'BLUE')), int(parser.get('OPENCV', 'GREEN')), int(parser.get('OPENCV', 'RED')))
+config['EMOTION_COLORS'] = list(json.loads(parser.get('OPENCV', 'EMOTION_COLORS')))
+config['MOOD_COLORS'] = list(json.loads(parser.get('OPENCV', 'MOOD_COLORS')))
 config['RECTANGLE_THICKNESS'] = int(parser.get('OPENCV', 'RECTANGLE_THICKNESS'))
 config['TEXT_THICKNESS'] = int(parser.get('OPENCV', 'TEXT_THICKNESS'))
 
 ############################################################################################################################################
 
-video_access(config, 0)
+video_stream(config)
